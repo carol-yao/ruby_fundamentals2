d9d40b49d2a2e5caaf75ede74299e3b8c0785a42
Carol Yao <c.y-@hotmail.com>
2016-06-01 14:12:40 -0400

    Added method for Celsius conversion

--- exercise5.rb
+++ exercise5.rb
@@ -1,2 +1,6 @@
 puts "What temperature would you like to convert from Fahrenheit to Celsius?"
 temperature = gets.chomp.to_i
+
+def Fahrenheit_to_Celsius(temperature)
+  (temperature - 32) * (5 / 9)
+end
