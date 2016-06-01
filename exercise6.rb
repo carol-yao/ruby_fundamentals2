1840031c73ac88f7f63fee9077f7ff49c5c79f40
Carol Yao <c.y-@hotmail.com>
2016-06-01 14:50:35 -0400

    Added rice to list and rewrote into method

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,10 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-
-grocery_list.each do |item|
-  puts "* #{item}"
+def output_list(list)
+  list.each do |item|
+    puts "* #{item}"
+  end
 end
+
+
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list << "rice"
+output_list(grocery_list)
