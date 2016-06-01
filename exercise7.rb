673b142c484aa33a6a761607718893c5e7e22f83
Carol Yao <c.y-@hotmail.com>
2016-06-01 15:18:02 -0400

    Outputted all cohort names

--- exercise7.rb
+++ exercise7.rb
@@ -1,9 +1,11 @@
+# 1
 students = {
             cohort1: 34,
             cohort2: 42,
             cohort3: 22
           }
 
+# 2
 def display(hash)
   hash.each do |key, value|
     puts "#{key}: #{value} students"
@@ -12,6 +14,8 @@ end
 
 display(students)
 
+# 3
 students[:cohort4] = 43
 
-puts students
+# 4
+puts students.keys
