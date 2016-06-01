de19924658df0ccbd5837756a901c78a6a5aeefa
Carol Yao <c.y-@hotmail.com>
2016-06-01 14:23:56 -0400

    Fixed method conversion problem and added return statment

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,9 @@
 puts "What temperature would you like to convert from Fahrenheit to Celsius?"
 temperature = gets.chomp.to_i
 
-def Fahrenheit_to_Celsius(temperature)
-  (temperature - 32) * (5 / 9)
+def fahrenheit_to_celsius(temp)
+  puts temp
+  (temp - 32) * (5.0 / 9.0)
 end
+
+p "That is #{fahrenheit_to_celsius(temperature)} Celsius"
