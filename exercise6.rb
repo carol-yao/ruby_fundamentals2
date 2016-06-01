352d616bf514e030f64db50fc49bf1ba200fc189
Carol Yao <c.y-@hotmail.com>
2016-06-01 14:54:24 -0400

    Checked if list contained bananas

--- exercise6.rb
+++ exercise6.rb
@@ -9,4 +9,10 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 grocery_list << "rice"
 output_list(grocery_list)
 
-puts grocery_list.length
+puts "There are #{grocery_list.length} items in the grocery list."
+
+if grocery_list.include?("bananas")
+  puts "You don't need to pick up bananas today."
+else
+  puts "You need to pick up bananas."
+end
