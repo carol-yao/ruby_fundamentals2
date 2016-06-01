b4e619017c21e2ae1b72246a99763b823a6494ef
Carol Yao <c.y-@hotmail.com>
2016-06-01 15:15:08 -0400

    Created method to display information in students hash

--- exercise7.rb
+++ exercise7.rb
@@ -1,7 +1,13 @@
-student = {
+students = {
             cohort1: 34,
             cohort2: 42,
             cohort3: 22
           }
 
-          
+def display(hash)
+  hash.each do |key, value|
+    puts "#{key}: #{value} students"
+  end
+end
+
+display(students)
